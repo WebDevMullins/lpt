f8590be8d426279af5f1a7f5e2f00e9aea9aa090
Brandon Mullins <webdevmullins@gmail.com>
2024-08-08 18:15:56 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-center'>
+			<h1 className='text-pretty text-4xl font-extrabold tracking-tight sm:text-[5rem]'>
+				Landscape Professionals of Texas
+			</h1>
+		</main>
+	)
+}
